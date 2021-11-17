8e726935261a2c02ec5805c765f680bfea1ce047
MinhPythonk <phamhoangminh672@gmail.com>
2021-11-17 21:57:31 +0700

    update _debug.py

--- _debug.py
+++ _debug.py
@@ -1,16 +1,22 @@
 import pygame
 
-def show_info(info, y_pos = 10, x_pos = 10):
+
+def show_info(info: str, y_pos=10, x_pos=10):
+    """
+    Shows a piece of information onto the display surface
+    This helps you to track the change of variables
+    """
     screen = pygame.display.get_surface()
     font = pygame.font.Font(None, 30)
-    info_text_surf = font.render(str(info), True, 'White')
+    info_text_surf = font.render(str(info), True, (255, 255, 255))
     screen.blit(info_text_surf, (x_pos, y_pos)) 
 
 
 def show_cursor():
+    """Show the current position of the mouse cursor in real time"""
     screen = pygame.display.get_surface()
     font = pygame.font.Font(None, 30)
     pos_list = [pos for pos in pygame.mouse.get_pos()]
-    info_text_surf = font.render(f'x: {pos_list[0]} y: {pos_list[1]}', True, 'White')
-    screen.blit(info_text_surf, (10, 50))
+    cursor_pos_surf = font.render(f'x: {pos_list[0]} y: {pos_list[1]}', True, (255, 255, 255))
+    screen.blit(cursor_pos_surf, (10, 50))
     

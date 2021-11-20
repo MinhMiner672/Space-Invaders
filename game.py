4c7e6d84bea30ed2b38b82becc0acef5c6f822ad
MinhPythonk <phamhoangminh672@gmail.com>
2021-11-20 10:18:05 +0700

    update game.py

--- game.py
+++ game.py
@@ -1,27 +1,27 @@
 import pygame
-import _global
 from sprites import SpaceShip, Rect, Bullet
 import colors
 
 
 class Game:
     """Main Game Class"""
+
     def __init__(self):
         # Basic stuff for the screen
         self.screen = pygame.display.set_mode((800, 600))
-        self.background = pygame.image.load('Images/bg.jpg').convert()
+        self.background = pygame.image.load('Images/Game/game_background.jpg').convert()
         self.clock = pygame.time.Clock()
         pygame.display.set_caption('Space Invaders')
-        pygame.display.set_icon(pygame.image.load('Images/game_icon.png').convert_alpha())
+        pygame.display.set_icon(pygame.image.load('Images/Game/game_icon.png').convert_alpha())
 
         # Game states
         self.started = False
         self.over = False
+        self.paused = False
         self.game_score = 0
 
         # Sprite Groups
         self.player_grp = pygame.sprite.GroupSingle(SpaceShip())
-        _global.player = self.player_grp
 
         self.bullet_grp = pygame.sprite.Group()
         self.power_type = 0
@@ -30,9 +30,9 @@ class Game:
         self.mana_grp = pygame.sprite.Group()
         self.health_cells_grp = pygame.sprite.Group()
         self.health_items = pygame.sprite.Group()
+        self.button_grp = pygame.sprite.Group()
 
         self.list_of_power_ups = pygame.sprite.Group()
-        self.activated_power_up = pygame.sprite.GroupSingle()
 
         self.health_top_left_x_pos = 497
         self.x_pos_for_each_power_up_in_list = 200
@@ -51,7 +51,7 @@ class Game:
         score_text_rect = text_score_surf.get_rect(topleft=(20, 540))
         self.screen.blit(text_score_surf, score_text_rect)
 
-    def refresh(self):
+    def refresh(self, event_class):
         """Cleans the game"""
         # clear all bullets and enemies
         self.bullet_grp.empty()
@@ -61,17 +61,19 @@ class Game:
         # recover the player (full 5 health cells)
         self.health_top_left_x_pos = 497
         for i in range(5):
-            self.health_cells_grp.add(Rect(x=self.health_top_left_x_pos, y=580, surf_type='health_cell', width=60, height=15))
+            self.health_cells_grp.add(
+                Rect(x=self.health_top_left_x_pos, y=580, surf_type='health_cell', event_class=event_class, width=60, height=15))
             self.health_top_left_x_pos += 60
         self.health_top_left_x_pos -= 60
 
-    def style_health_bar(self):
+    def style_health_bar(self, event_class):
+        """Changes the health bar's color depending on the number of health cells"""
         if 1 < len(self.health_cells_grp) < 4:
-            _global.rect_health_color = colors.YELLOW
+            event_class.rect_health_color = colors.YELLOW
         elif len(self.health_cells_grp) == 1:
-            _global.rect_health_color = colors.RED
+            event_class.rect_health_color = colors.RED
         elif len(self.health_cells_grp) >= 4:
-            _global.rect_health_color = colors.GREEN
+            event_class.rect_health_color = colors.GREEN
 
     def control_fps(self):
         """Controls the FPS's consistence, make it not fluctuate"""

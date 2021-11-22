1e0331956463b8e3550fd0af18bc813d4506e9f7
MinhPythonk <phamhoangminh672@gmail.com>
2021-11-22 13:59:55 +0700

    update sound.py

--- sound.py
+++ sound.py
@@ -5,15 +5,16 @@ class SoundPlayer:
         self.shoot = mixer.Sound('./Sound/shoot.wav')
         self.activate = mixer.Sound('./Sound/activation.wav')
         self.heal = mixer.Sound('./Sound/heal.wav')
-        # self.player_touches_an_enemy = mixer.Sound('')
-        self.an_enemy_dies = mixer.Sound('./Sound/roblox_die.mp3')
-        # self.game_over = mixer.Sound('')
-        # self.touches_an_bullet = mixer.Sound('')
-        # self.get_score = mixer.Sound('')
+        self.player_touches_an_enemy = mixer.Sound('./Sound/lost_1_health.mp3')
+        self.an_enemy_dies = mixer.Sound('./Sound/kill_an_enemy.mp3')
+        self.game_over = mixer.Sound('./Sound/game_over.mp3')
 
         self.shoot.set_volume(0.1)
         self.activate.set_volume(1)
         self.an_enemy_dies.set_volume(0.2)
+        self.player_touches_an_enemy.set_volume(0.5)
+        self.heal.set_volume(0.5)
+        self.game_over.set_volume(1)
 
     
 
